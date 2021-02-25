1c8998710c4d7733c0e04a75f08818ff95d0e380
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-02-26 04:12:03 +0530

    Disply Welcome program

---
+++ EmpWageBuilder.java
@@ -0,0 +1,17 @@
+public class EmpWageBuilder {
+
+	public static void main(String args[]){
+
+		DispWelcome  disp = new DispWelcome();
+
+		disp.display();
+		
+	}
+
+	public void display() {
+
+		System.out.println("Welcome to Employee Wage Computation");
+
+	}
+
+}
